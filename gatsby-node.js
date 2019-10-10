4ba7a8663e20a1e90b60a2a9c67c16ef7eae71f0
nikpak78 <nikpak78@gmail.com>
2019-10-10 13:59:49 +0300

    second commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
 
-// You can delete this file if you're not using it
+
+const path = require('path');
+const { paginate } = require('gatsby-awesome-pagination');
+
+
+exports.createPages = async ({ actions, graphql }) => {
+ const { createPage } = actions;
+ 
+ const articles = await graphql(`
+   {
+     allNodeArticle {
+       nodes {
+         id
+         title
+         path {
+           alias
+         }
+       }
+     }
+   }
+ `);
+
+
+ paginate({
+  createPage,
+  items: articles.data.allNodeArticle.nodes,
+  itemsPerPage: 5,
+  pathPrefix: '/blog',
+  component: path.resolve(`src/templates/blog.js`),
+});
+ 
+ articles.data.allNodeArticle.nodes.map(articleData =>
+   createPage({
+     path: articleData.path.alias,
+     component: path.resolve(`src/templates/article.js`),
+     context: {
+       ArticleId: articleData.id,
+     },
+   })
+ );
+}
+
--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,16 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem`}}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `coral`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -27,6 +33,11 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/blog/">Blog</ListLink>
+        <ListLink to="/articles/">Articles</ListLink>
+      </ul>
     </div>
   </header>
 )
